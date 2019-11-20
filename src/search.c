892237575918778f8c45a0dd8b5c29dceeab66be
Alayan <alayan.feh@gmail.com>
2019-11-21 00:29:56 +0100

    Singular first

--- src/search.c
+++ src/search.c
@@ -494,9 +494,14 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
         // an early move has excellent continuation history, or when we have a move
         // from the transposition table which appears to beat all other moves by a
         // relativly large margin,
-        extension =  (inCheck)
-                  || (isQuiet && quietsSeen <= 4 && cmhist >= 10000 && fmhist >= 10000)
-                  || (singular && moveIsSingular(thread, ttMove, ttValue, depth, height));
+        extension = 0;
+        if (singular && moveIsSingular(thread, ttMove, ttValue, depth, height)) {
+            extension = 1;
+        }
+        else if (   (inCheck)
+                 || (isQuiet && quietsSeen <= 4 && cmhist >= 10000 && fmhist >= 10000)) {
+            extension = 1;
+        }
 
         // Factor the extension into the new depth. Do not extend at the root
         newDepth = depth + (extension && !RootNode);
