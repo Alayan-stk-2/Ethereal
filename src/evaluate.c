30c05b6d1d8c2940ab7a8e32fc13992cead7646d
Alayan <alayan.feh@gmail.com>
2019-12-08 21:50:26 +0100

    With compensation

--- src/evaluate.c
+++ src/evaluate.c
@@ -274,7 +274,7 @@ const int KSSafeQueenCheck  =   95;
 const int KSSafeRookCheck   =   94;
 const int KSSafeBishopCheck =   51;
 const int KSSafeKnightCheck =  123;
-const int KSAdjustment      =  -18;
+const int KSAdjustment      =  -25;
 
 /* Passed Pawn Evaluation Terms */
 
