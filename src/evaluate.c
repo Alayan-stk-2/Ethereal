8002b65cdfd9fcd03f757877d61036b46448cde3
Alayan <alayan.feh@gmail.com>
2019-12-08 18:37:12 +0100

    Take 5

--- src/evaluate.c
+++ src/evaluate.c
@@ -269,9 +269,9 @@ const int KSAttackValue     =   44;
 const int KSWeakSquares     =   38;
 const int KSFriendlyPawns   =  -22;
 const int KSNoEnemyQueens   = -276;
-const int KSSafeQueenCheck  =  103;
-const int KSSafeRookCheck   =  124;
-const int KSSafeBishopCheck =   63;
+const int KSSafeQueenCheck  =  109;
+const int KSSafeRookCheck   =  135;
+const int KSSafeBishopCheck =   72;
 const int KSSafeKnightCheck =  123;
 const int KSAdjustment      =  -18;
 
