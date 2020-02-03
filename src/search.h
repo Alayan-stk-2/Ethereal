75730b35c34f6293ad39367c007868ea90660dda
Alayan <alayan.feh@gmail.com>
2020-02-03 20:38:01 +0100

    Higher cmh/fmp depth

--- src/search.h
+++ src/search.h
@@ -66,10 +66,10 @@ static const int FutilityMarginNoHistory = 256;
 static const int FutilityPruningDepth = 8;
 static const int FutilityPruningHistoryLimit[] = { 12000, 6000 };
 
-static const int CounterMovePruningDepth[] = { 3, 2 };
+static const int CounterMovePruningDepth[] = { 4, 3 };
 static const int CounterMoveHistoryLimit[] = { 0, -1000 };
 
-static const int FollowUpMovePruningDepth[] = { 3, 2 };
+static const int FollowUpMovePruningDepth[] = { 4, 3 };
 static const int FollowUpMoveHistoryLimit[] = { -2000, -4000 };
 
 static const int LateMovePruningDepth = 8;
