18ebeb29872250e02380f6beeedf2884a571cb47
Alayan <alayan.feh@gmail.com>
2020-02-06 23:34:08 +0100

    Lower values

--- src/search.h
+++ src/search.h
@@ -54,7 +54,7 @@ static const int WindowTimerMS = 2500;
 static const int CurrmoveTimerMS = 2500;
 
 static const int BetaPruningDepth = 8;
-static const int BetaMargin[9] =  { 0,120,220,300,375,445,515,585,655 };
+static const int BetaMargin[9] =  { 0,100,190,270,345,415,485,555,625 };
 
 static const int NullMovePruningDepth = 2;
 
