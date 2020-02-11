e0396c45137405e14b3d9fe16c8f3a54a0462c50
Alayan <alayan.feh@gmail.com>
2020-02-11 06:57:28 +0100

    Margin 135

--- src/search.h
+++ src/search.h
@@ -59,7 +59,7 @@ static const int BetaMargin = 85;
 static const int NullMovePruningDepth = 2;
 
 static const int ProbCutDepth = 5;
-static const int ProbCutMargin = 65;
+static const int ProbCutMargin = 135;
 
 static const int FutilityMargin = 90;
 static const int FutilityMarginNoHistory = 256;
